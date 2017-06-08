b0bd3f6a3bba852aae55bb201d5627031f57b0db
RubenHernandez184 <Ruben.Hernandez@kippnyccp.org>
2017-06-08 13:39:53 -0400

    Create application.js

--- Guess/application.js
+++ Guess/application.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
-	$("id=submit button").click(function(){
- 
- 
- 
-	$("id=clear button").click(function(){		
- 
+	$("#submit").click(function(){
+		//what did they guess?
+		var guess =
+		if(guess == random_num){
+		} else if (){
+		}
+	});	
+ 	//$("id=clear button").click(function(){		
+ 	//	var number= $("input[name=number"]").val();
  
 });
